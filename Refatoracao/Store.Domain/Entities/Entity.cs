fd158a7b1d77ec39078a8bf3dd16b6e1be522b44
Anderson <anderson@prodwin.com.br>
2020-06-18 19:08:26 -0300

    Implementado os Domínios
    Implementado os Domínios

---
+++ Refatoracao/Store.Domain/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Domain.Entities {
+	public class Entity {
+
+		public Entity() {
+
+			Id = Guid.NewGuid();
+		}
+
+		public Guid Id { get; private set; }
+
+	}
+}
