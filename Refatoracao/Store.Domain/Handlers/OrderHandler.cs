53a969b73f0b176dfd1fb5a59dc828627e7722c8
Anderson <anderson@prodwin.com.br>
2020-06-19 23:15:31 -0300

    Implementado os Handlers e Testes
    Implementado os Handlers e Testes

---
+++ Refatoracao/Store.Domain/Handlers/OrderHandler.cs
@@ -0,0 +1,64 @@
+﻿using Flunt.Notifications;
+using Store.Domain.Commands;
+using Store.Domain.Commands.Interfaces;
+using Store.Domain.Entities;
+using Store.Domain.Repositories.Interfaces;
+
+namespace Store.Domain.Handlers {
+	public class OrderHandler: Notifiable,
+		IHandler<CreateOrderCommand> {
+
+		private readonly ICustomerRepository _customerRepository;
+		private readonly IDeliveryFeeRepository _deliveryFeeRepository;
+		private readonly IDiscountRepository _discountRepository;
+		private readonly IProductRepository _produtoRepository;
+		private readonly IOrderRepository _orderRepository;
+
+		public OrderHandler(ICustomerRepository customerRepository, 
+							IDeliveryFeeRepository deliveryFeeRepository, 
+							IDiscountRepository discountRepository, 
+							IProductRepository produtoRepository, 
+							IOrderRepository orderRepository) {
+
+			_customerRepository = customerRepository;
+			_deliveryFeeRepository = deliveryFeeRepository;
+			_discountRepository = discountRepository;
+			_produtoRepository = produtoRepository;
+			_orderRepository = orderRepository;
+		}
+
+		public ICommandResult Handle(CreateOrderCommand command) {
+
+			command.Validate();
+			if(command.Valid)
+				return new GenericsCommandResult(false, "Pedido invalido", null);
+
+			//Recupera o cliente
+			var customer = _customerRepository.Get(command.Customer);
+
+			//Calcula a taxa de entrega
+			var deliveryFee = _deliveryFeeRepository.Get(command.ZipCode);
+
+			// Obtem o cupom de desconto
+			var discount = _discountRepository.Get(command.PromoCode);
+
+			// Gera o pedido
+
+			var products = _produtoRepository.Get(ExtractGuids.Extract(command.Items)).ToList();
+			var order = new Order(customer, deliveryFee, discount);
+			foreach(var item in command.Items) {
+				var product = products.Where(x => x.Id == item.Product).FirstOrDefault();
+				order.AddItem(product, item.Quantity);
+			}
+
+			//AddNotifications(customer.Notifications);
+			AddNotifications(order.Notifications);
+
+			if(Invalid)
+				return new GenericsCommandResult(false, "Falha ao gerar o pedido", Notifications);
+
+			_orderRepository.Save(order);
+			return new GenericsCommandResult(true, $"Pedido {order.Number} gerado com sucesso", order);
+		}
+	}
+}
---
+++ Refatoracao/Store.Tests/Handler/OrderHandlerTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+using Store.Domain.Handlers;
+using Store.Domain.Repositories.Interfaces;
+using Store.Tests.Repositories;
+using System;
+
+namespace Store.Tests.Handler {
+	[TestClass]
+	public class OrderHandlerTest {
+
+		private readonly ICustomerRepository _customerRepository;
+		private readonly IDeliveryFeeRepository _deliveryFeeRepository;
+		private readonly IDiscountRepository _discountRepository;
+		private readonly IProductRepository _produtoRepository;
+		private readonly IOrderRepository _orderRepository;
+
+		public OrderHandlerTest() {
+			_customerRepository = new FakeCustomerRepository();
+			_deliveryFeeRepository = new FakeDeliveryFeeRepository();
+			_discountRepository = new FakeDiscountRepository();
+			_produtoRepository = new FakeProductRepository();
+			_orderRepository = new FakeOrderRepository();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_cliente_inexistente_o_pedido_nao_deve_ser_gerado() {
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_cep_invalido_o_pedido_deve_ser_gerado_normalmente() {
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_promocode_inexistente_o_pedido_deve_ser_gerado_normalmente() {
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_pedido_sem_itens_o_mesmo_nao_deve_ser_gerado() {
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado() {
+
+			Assert.Fail();
+		}
+
+		[TestMethod]
+		[TestCategory("Handlers")]
+		public void Dado_um_comando_valido_o_pedido_deve_ser_gerado() {
+
+			var command = new CreateOrderCommand();
+
+			command.Customer = "12345678";
+			command.ZipCode = "13411080";
+			command.PromoCode = "12345678";
+			command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+			command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+			var handler = new OrderHandler(_customerRepository, _deliveryFeeRepository, _discountRepository, _produtoRepository, _orderRepository);
+			handler.Handle(command);
+			Assert.AreEqual(handler.Valid, true);
+
+		}
+
+	}
+}
